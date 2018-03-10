3571e54d21f047e34d00a68df6178f3612df8731
chinnonae <chinnonae@gmail.com>
2018-03-10 17:07:15 +0700

    edit database configuration

--- config/config.js
+++ config/config.js
@@ -1,18 +1,18 @@
 let username = require('os').userInfo().username
 
 module.exports = {
-  production: {
-    username: process.env.POSTGRES_DB_USER,
-    password: process.env.POSTGRES_DB_PASSWORD,
-    host: process.env.POSTGRES_DB_HOST,
-    database: 'lenscape',
-    dialect: 'postgres'
-  },
   development: {
     username,
     password: '1234',
     database: 'lenscape_dev',
     host: 'localhost',
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.POSTGRES_DB_USER,
+    password: process.env.POSTGRES_DB_PASSWORD,
+    host: 'localhost',
+    database: 'lenscape_prod',
     dialect: 'postgres'
   }
 }
