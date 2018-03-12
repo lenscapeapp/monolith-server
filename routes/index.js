6d0fa0a4cd4fc0f7c6d869e7e61aa4d6af97a1c9
chinnonae <chinnonae@gmail.com>
2018-03-12 16:40:43 +0700

    remove conflicted file

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const authRouter = require('./auth')
 const router = new Router()
 
 router.use('/', authRouter)
-router.use('/', require('./test-upload'))
 
 router.get('/', (req, res) => {
   res.json({ message: 'Health check' })
