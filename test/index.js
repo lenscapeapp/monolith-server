342500a06ed7eb8238685fd7c18bf8f08bf1ba37
chinnonae <chinnonae@gmail.com>
2018-03-30 15:17:08 +0700

    sync database for ci

--- test/index.js
+++ test/index.js
@@ -4,11 +4,16 @@ process.env.ENV = 'test'
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const server = require('../server')
+const { sequelize } = require('../models')
 
 chai.should()
 chai.use(chaiHttp)
 
 describe('Health check', () => {
+  before(() => {
+    sequelize.sync()
+  })
+
   it('should return 200', (done) => {
     chai.request(server)
       .get('/')
