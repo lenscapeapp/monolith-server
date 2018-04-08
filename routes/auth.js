7a871b26c5b2a4faf3c184ef2652a4a1aaedcc37
chinnonae <chinnonae@gmail.com>
2018-04-08 22:51:38 +0700

    refactor

--- routes/auth.js
+++ routes/auth.js
@@ -1,75 +1,21 @@
 const bcrypt = require('bcrypt')
 const { Router } = require('express')
 
+const Authentication = require('../middlewares/authentication')
 const { Facebook, Auth } = require('../functions')
 const { FacebookAuth, LocalAuth, User, sequelize } = require('../models')
 
 const router = new Router()
 
 router.post('/register',
-  async (req, res, next) => {
-    const { firstname, lastname, password, email } = req.body
-    let hpassword = await bcrypt.hash(password, 10)
-
-    await sequelize.transaction(async function (t) {
-      try {
-        let localauth = await LocalAuth.create({
-          hpassword,
-          User: { firstname, lastname, email }
-        }, {
-          transaction: t,
-          include: [{ association: LocalAuth.associations.User }]
-        })
-
-        req.user = localauth.User
-
-        if (req.file !== undefined) {
-          let extension = req.file.mimetype.split('/')[1]
-          let photo = await req.user.createCurrentProfilePhoto({
-            type: 'profile',
-            extension,
-            name: '',
-            owner_id: req.user.id
-          }, { transaction: t })
-          let pictureUrls = await photo.upload(req.file, req.file.mimetype)
-          req.userPicture = pictureUrls.thumbnail
-        }
-        next()
-      } catch (error) {
-        console.log(error)
-        res.statusCode = 500
-        next(error)
-        throw error
-      }
-    })
-  },
-  Auth.authorize
+  Authentication.register,
+  Authentication.authorize
 )
 
-router.post('/login/local', async (req, res, next) => {
-  const { email, password } = req.body
-
-  try {
-    req.user = await User.findOne({
-      where: { email },
-      include: [{ model: LocalAuth }]
-    })
-  } catch (error) {
-    res.statusCode = 500
-    return next(error)
-  }
-
-  if (req.user === null || req.user.LocalAuth === null) {
-    return res.status(401).json({ message: 'Email or Password is incorrect' })
-  }
-
-  let authenticated = await bcrypt.compare(password, req.user.LocalAuth.hpassword)
-  if (!authenticated) {
-    return res.status(401).json({ message: 'Email or Password is incorrect' })
-  }
-
-  next()
-}, Auth.authorize)
+router.post('/login/local',
+  Authentication.loginLocal,
+  Authentication.authorize
+)
 
 router.post('/login/facebook', async (req, res, next) => {
   try {
