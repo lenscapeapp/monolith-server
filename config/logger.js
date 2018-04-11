03d11c4bbac32e775c4edf648bf95412c53cef74
chinnonae <chinnonae@gmail.com>
2018-04-11 18:16:33 +0700

    sequelize use winston logger

--- config/logger.js
+++ config/logger.js
@@ -1,24 +1,37 @@
 const winston = require('winston')
+const CircularJSON = require('circular-json')
 
 const { LOG_LEVEL } = require('../config/constants')
 
-const idx = (o, ...p) => p.reduce((xs, x) => (xs && xs[x]) ? xs[x] : undefined, o)
+function replaceBuffer (name, val) {
+  if (val && val.type && val.type === 'Buffer' && val.data instanceof Array) {
+    return '<Buffer>'
+  }
+  return val
+}
+
+const ReplaceBufferFormatter = winston.format((info, opts) => {
+  let formData = info.options && info.options.formData
+  if (formData && (formData.file instanceof Buffer)) {
+    formData.file = formData.file.inspect()
+  }
+  return info
+})
+
+const CircularJsonFormatter = winston.format((info, opts) => {
+  info[Symbol.for('message')] = CircularJSON.stringify(info, replaceBuffer)
+  return info
+})
 
 const logger = winston.createLogger({
   level: LOG_LEVEL,
   format: winston.format.combine(
-    winston.format((info, opts) => {
-      if (info instanceof Error) {
-        let formData = idx(info, 'options', 'formData')
-        if (formData && formData.file === undefined) return info
-        formData.file = formData.file.inspect()
-      }
-      return info
-    })(),
+    ReplaceBufferFormatter(),
     winston.format.timestamp(),
-    winston.format.json()
+    CircularJsonFormatter()
   ),
   transports: [new winston.transports.Console()]
+  // transports: [new winston.transports.File({ filename: 'test.log' })]
 })
 
 module.exports = logger
